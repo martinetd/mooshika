9d16e9287712c09acb622bcdae74577ee7975810
Dominique Martinet <dominique.martinet@cea.fr>
2012-06-11 18:06:33 +0200

    filled lots of things, nothing's gonna work!

--- trans_rdma.h
+++ trans_rdma.h
@@ -37,7 +37,7 @@ QUESTIONS:
 		log_level_t? easy to use out of nfs-ganesha? Might want just to ceate dummy macroes for now
  - callbacks
  - ok to use pthread, or does nfs-ganesha have any specific ones? (same with semaphores/lock, pthread_cond_*?) 
-	-> looks ok with pthread_conf or include/SemN.h
+	-> looks ok with pthread_cond or include/SemN.h
  - queue implem to use, make my own? easy way out = like diod = a fixed length array of bufers with a "used" flag
 ==========================================
 not a question, but 9p only uses recv/send and never _ever_ does any read/write in its current implementation...
@@ -45,10 +45,10 @@ not a question, but 9p only uses recv/send and never _ever_ does any read/write 
 
 // public api;
 
-typedef struct rdma_data rdma_data;
-typedef struct rdma_trans rdma_trans;
-typedef struct rdma_ctx rdma_ctx;
-typedef struct rdma_rloc rdma_rloc;
+typedef struct rdma_data rdma_data_t;
+typedef struct rdma_trans rdma_trans_t;
+typedef struct rdma_ctx rdma_ctx_t;
+typedef struct rdma_rloc rdma_rloc_t;
 
 /**
  * \struct rdma_data
@@ -65,36 +65,34 @@ struct rdma_data {
  */
 struct rdma_trans {
 	enum {
-		RCAT_RDMA_INIT,
-		RCAT_RDMA_ADDR_RESOLVED,
-		RCAT_RDMA_ROUTE_RESOLVED,
-		RCAT_RDMA_CONNECTED,
-		RCAT_RDMA_FLUSHING,
-		RCAT_RDMA_CLOSING,
-		RCAT_RDMA_CLOSED,
+		RDMA_INIT,
+		RDMA_ADDR_RESOLVED,
+		RDMA_ROUTE_RESOLVED,
+		RDMA_CONNECTED,
+		RDMA_FLUSHING,
+		RDMA_CLOSING,
+		RDMA_CLOSED,
 	} state;			/**< tracks the transport state machine for connection setup and tear down */
 	struct rdma_cm_id* cm_id;	/**< The RDMA CM ID */
+	struct rdma_event_channel* event_channel;
 	struct ib_pd* pd;		/**< Protection Domain pointer */
 	struct ib_qp* qp;		/**< Queue Pair pointer */
 	struct ib_cq* cq;		/**< Completion Queue pointer */
 	struct ib_mr* dma_mr;		/**< DMA Memory Region pointer */
 	uint32_t lkey;			/**< The local access only memory region key */
-	long timeout;			/**< Number of uSecs to wait for connection management events */
+	long timeout;			/**< Number of mSecs to wait for connection management events */
 	int sq_depth;			/**< The depth of the Send Queue */
 	struct semaphore sq_sem;	/**< Semaphore for the SQ */
 	int rq_depth;			/**< The depth of the Receive Queue. */
-	atomic_t rq_count;		/**< Count of requests in the Receive Queue. */
-	struct sockaddr_in addr;	/**< The remote peer's address */
-	spinlock_t req_lock;		/**< Protects the active request list */
-
-	struct completion cm_done;	/**< Completion event for connection management tracking */
-
+	int rq_count;			/**< Count of requests in the Receive Queue. */
+	struct sockaddr_storage addr;	/**< The remote peer's address */
+//TODO: fill this, remember to init stuff.
 };
 
 
 /**
  * \struct rdma_ctx
- * Context data we can use during recv/send callbacks
+ *l Context data we can use during recv/send callbacks
  */
 struct rdma_ctx {
 	int used;			/**< 0 if we can use it for a new recv/send */
@@ -113,16 +111,16 @@ struct rdma_ctx {
  * stores one remote address to write/read at
  */
 struct rdma_rloc {
-	uint64_t addr; /**< remote address */
+	uint64_t rmemaddr; /**< remote address */
 	uint32_t rkey; /**< remote key */
 	uint32_t size; /**< size of the region we can write/read */
 }
 
 
-int rdma_recv(rdmatrans* trans, u32 msize, void (*callback)(rdma_data* data)); // or give trans a recv function an' do trans->recv
+int rdma_recv(rdmatrans* trans, uint32_t msize, void (*callback)(rdma_data* data)); // or give trans a recv function an' do trans->recv
 int rdma_send(rdmatrans* trans, rdma_data* data, void (*callback)(void));
 
-int rdma_recv_wait(rdmatrans* trans, rdma_data** datap, u32 msize);
+int rdma_recv_wait(rdmatrans* trans, rdma_data** datap, uint32_t msize);
 int rdma_send_wait(rdmatrans* trans, rdma_data* data);
 
 // server side
@@ -132,6 +130,21 @@ int rdma_read(trans, rdma_rloc, size);
 // client side
 int rdma_write_request(trans, rdma_rloc, size); // = ask for rdma_write server side ~= rdma_read
 int rdma_read_request(trans, rdma_rloc, size); // = ask for rdma_read server side ~= rdma_write
+
+struct ibv_mr* register_mr(rdma_trans_t* trans, void* memaddr, size_t size, int access);
+int deregister_mr(struct ibv_mr* mr);
+
+rdma_trans_t* rdma_make_rkey(uint64_t addr, ibv_mr* mr, uint32_t size);
+
+// server specific:
+rdma_trans_t* rdma_create(sockaddr_storage* addr);
+rdma_trans_t* rdma_accept_one(rdma_trans_t* trans);
+int rdma_destroy(rdma_trans_t* rdma_trans);
+// do we want create/destroy + listen/shutdown, or can both be done in a single call?
+// if second we could have create/destroy shared with client, but honestly there's not much to share...
+// client
+rdma_trans_t* rdma_connect(sockaddr_storage* addr);
+int rdma_disconnect(rdma_trans_t* trans);
 
 
 
