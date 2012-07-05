21600268a4c55ab7ba7b492edb49f8d673c8823c
Dominique Martinet <dominique.martinet@cea.fr>
2012-07-05 17:08:48 +0200

    changed way of building rpm so it works with rpmbuild --rebuild libercat*.srpm too......... changed autogen.sh so it doesn't run configure

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,10 @@
 
 set -e
 
-autoreconf -i -f
-./configure "$@"
+#autoreconf -i -f
+rm -f config.cache
+aclocal -I m4
+libtoolize --force --copy
+autoconf
+autoheader
+automake -a --add-missing -Wall
